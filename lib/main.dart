04b050a33b6613889547d6314076e89416a4658a
gauravsuryvanshi06 <gauravsuryvanshi06@gmail.com>
2023-03-24 21:04:05 +0530

    Committed Work

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Navigation',
+    theme: ThemeData(
+      // This is the theme of your application.
+      primarySwatch: Colors.green,
+    ),
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: MaterialButton(
+          child: Text('Click Here'),
+          color: Colors.orangeAccent,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: MaterialButton(
+          color: Color.fromARGB(255, 0, 0, 0),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back'),
+        ),
+      ),
+    );
+  }
+}
